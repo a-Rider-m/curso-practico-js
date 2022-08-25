5fcc8c65a6eb153a605d343dbfc0b80605715e73
Miguel Angel Arias <miguel.a.frontend@gmail.com>
2022-08-24 20:34:14 -0500

    agregar product detail shoppingcart

--- curso-2/main.js
+++ curso-2/main.js
@@ -1,7 +1,11 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-const mobileMenu = document.querySelector(".mobile-menu");
+
 const burgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
@@ -13,4 +17,10 @@ burgerMenu.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle("inactive");
+}
+
+shoppingCart.addEventListener("click", toggleShoppingCart);
+
+function toggleShoppingCart() {
+    productDetail.classList.toggle("inactive");
 }
