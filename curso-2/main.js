ffb0c0b6412ac23d71a711a51faa4e2b41a5b314
Miguel Angel Arias <miguel.a.frontend@gmail.com>
2022-08-24 19:55:34 -0500

    Integrar menu mobile

--- curso-2/main.js
+++ curso-2/main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const burgerMenu = document.querySelector(".menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+
+burgerMenu.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
 }
