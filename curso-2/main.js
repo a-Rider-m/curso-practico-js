9f796360071d9d0987f371a22e554b9bd950762d
Miguel Angel Arias <miguel.a.frontend@gmail.com>
2022-08-25 20:15:52 -0500

    Solucionar problema de solapamiento navbar

--- curso-2/main.js
+++ curso-2/main.js
@@ -1,26 +1,48 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
-
-const burgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
+const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenu = document.querySelector(".menu");
 const shoppingCart = document.querySelector(".navbar-shopping-cart");
-const productDetail = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+burgerMenu.addEventListener("click", toggleMobileMenu);
+shoppingCart.addEventListener("click", toggleShoppingCart);
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+    
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
     desktopMenu.classList.toggle("inactive");
 }
 
-burgerMenu.addEventListener("click", toggleMobileMenu);
-
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+    
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
     mobileMenu.classList.toggle("inactive");
 }
 
-shoppingCart.addEventListener("click", toggleShoppingCart);
-
 function toggleShoppingCart() {
-    productDetail.classList.toggle("inactive");
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const isDeskTopMenuClosed = desktopMenu.classList.contains("inactive");
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+    }
+
+    if (!isDeskTopMenuClosed) {
+        desktopMenu.classList.add("inactive");
+    }
+    
+    aside.classList.toggle("inactive");
 }
+
+
